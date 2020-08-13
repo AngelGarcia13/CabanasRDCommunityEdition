40f243a07bf5db9af2b7f88db5e04d25da66f44a
Angel Garcia <angelrenegarcia13@gmail.com>
2020-08-13 21:54:29 +0200

    Added MotelSource with Refit and AutoMapper, added Mobile Build Tools
    Added MotelSource with Refit and AutoMapper, added Mobile Build Tools

---
+++ src/App/CabanasRD/Configs/AppSettingsConstants.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace CabanasRD.Configs
+{
+    public static class AppSettingsConstants
+    {
+        public const string ApiUrl = "https://cabanasrd-api.azurewebsites.net";
+        public const string ApiKey = "7L2mMPovIcaCz26BrDelJyBmjugdPycgaBwNxPlRRvK9ahxloUdOnQ==";
+
+    }
+}
---
+++ src/App/CabanasRD/Framework/APIs/Models/MotelResponse.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace CabanasRD.Framework.APIs.Models
+{
+    public partial class MotelResponse
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("longitude")]
+        public string Longitude { get; set; }
+
+        [JsonProperty("latitude")]
+        public string Latitude { get; set; }
+
+        [JsonProperty("motelServices")]
+        public List<MotelResponseService> MotelServices { get; set; }
+
+        [JsonProperty("phones")]
+        public List<string> Phones { get; set; }
+
+        [JsonProperty("takeCredictCards")]
+        public bool TakeCredictCards { get; set; }
+
+        [JsonProperty("ranking")]
+        public int Ranking { get; set; }
+
+        [JsonProperty("images")]
+        public List<Uri> Images { get; set; }
+
+        [JsonProperty("address")]
+        public string Address { get; set; }
+
+        [JsonProperty("state")]
+        public State State { get; set; }
+
+        [JsonProperty("descripcion")]
+        public object Descripcion { get; set; }
+
+        [JsonProperty("creditCards")]
+        public List<object> CreditCards { get; set; }
+
+        [JsonProperty("type")]
+        public int Type { get; set; }
+
+        [JsonProperty("isManagedByTheOwner")]
+        public bool IsManagedByTheOwner { get; set; }
+
+        [JsonProperty("rankingValue")]
+        public double RankingValue { get; set; }
+
+    }
+
+    public partial class MotelResponseService
+    {
+        [JsonProperty("service")]
+        public string Service { get; set; }
+
+        [JsonProperty("price")]
+        public double Price { get; set; }
+
+        [JsonProperty("type")]
+        public int Type { get; set; }
+
+        [JsonProperty("currencyType")]
+        public int CurrencyType { get; set; }
+
+        [JsonProperty("descriptionDetail")]
+        public object DescriptionDetail { get; set; }
+    }
+
+    public partial class State
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}
